3bd94d36992e614b0be15746d7618254fa19210e
Hendri Alqori <teamhendri18@gmail.com>
2024-10-22 20:40:23 +0700

    feat: create generateMD5 hash function

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,9 +1,21 @@
-import crypto from 'crypto'
+import crypto from 'node:crypto'
+import fs from "node:fs"
 import { Request, Response, NextFunction } from 'express'
 import winston from 'winston';
 
-export function random() {
-    return crypto.randomBytes(128).toString("base64")
+
+export const generateMD5 = (filePath: string): Promise<string>  => {
+    return new Promise((res, rej) => {
+        const hash = crypto.createHash('md5');
+        const rStream = fs.createReadStream(filePath);
+
+        rStream.on('data', (data) => {
+            hash.update(data);
+        });
+        rStream.on('end', () => {
+            res(hash.digest('hex'));
+        });
+    })
 }
 
 export function logger(req: Request, res: Response, next: NextFunction) {
