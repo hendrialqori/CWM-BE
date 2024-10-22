5fece2e330d87a40c125cdcc44e29748f3909f2d
Hendri Alqori <teamhendri18@gmail.com>
2024-10-22 20:39:39 +0700

    feat(service)!: implement upload zip file on add, update and delete product

--- src/services/products.service.ts
+++ src/services/products.service.ts
@@ -12,15 +12,17 @@ import { InsertProduct, Product } from '../types'
 import radash from 'radash'
 import { writeFile, unlink } from 'fs/promises'
 import path from 'path'
+import { generateMD5 } from 'src/utils/helpers'
 
 export default class ProductService {
+
     private static COLUMN = {
         id: productsTable.id,
         image: productsTable.image,
         title: productsTable.title,
         originalPrice: productsTable.originalPrice,
         strikeoutPrice: productsTable.strikeoutPrice,
-        description: productsTable.description
+        description: productsTable.description,
     }
 
     static async list() {
@@ -35,7 +37,7 @@ export default class ProductService {
     // only for administrator
     static async listPrivate() {
         const products = await db
-            .select({ ...ProductService.COLUMN, isOffer: productsTable.isOffer, link: productsTable.link })
+            .select({ ...ProductService.COLUMN, isOffer: productsTable.isOffer })
             .from(productsTable)
 
         return products
@@ -70,76 +72,113 @@ export default class ProductService {
 
     static async add(request: Request) {
         const body = request.body as InsertProduct
-        const image = request.file!
+        const imageFile = request.files['image'][0] as Express.Multer.File
+        const zipFile = request.files["zip"][0] as Express.Multer.File
 
-        //validation
+        //validation product body
         const productRequest = Validation.validate(ProductsValidation.ADD, body) as Product
 
-        // validate image
-        if (!radash.isObject(image)) {
-            throw new Error.FileUploadError(400, "Image required")
-        }
+        // validation image and zip
+        const isImage = radash.isObject(imageFile)
+        const isZip = radash.isObject(zipFile)
 
-        const imageName = Date.now() + "-" + image.originalname
-        const imageBuffer = image.buffer
+        // validate image and zip file
+        if (!isImage) throw new Error.FileUploadError(400, "Image required")
+        if (!isZip) throw new Error.FileUploadError(400, "Zip file required")
+
+        const imageName = Date.now() + "-" + imageFile.originalname
+        const imageBuffer = imageFile.buffer
         const imagePath = path.join(__dirname, "..", "..", "public", "static", imageName);
 
-        const newProduct = { ...productRequest, image: imageName }
+        const zipName = `${Date.now()}-${zipFile.originalname}`
+        const zipBuffer = zipFile.buffer
+        const zipPath = path.join(__dirname, "..", "..", "_zip", zipName);
 
+        // store image file into directory
         await writeFile(imagePath, imageBuffer);
+        // store zip file into directory
+        await writeFile(zipPath, zipBuffer)
 
         // if request body isOffer is true, force all data that have become isOffer=false
         if (productRequest.isOffer) {
             await ProductService.forceIsOfferEqualIsFalse()
         }
 
-        const insertNewProduct = await db
+        // store zip file into database
+        const zipMd5 = await generateMD5(zipPath)
+        const newProduct = {
+            ...productRequest,
+            image: imageName,
+            zipPath: zipName,
+            zipMd5
+        } as Product
+
+        // store into database
+        const [result] = await db
             .insert(productsTable)
             .values(newProduct)
             .$returningId()
 
-        return { ...insertNewProduct[0], ...newProduct }
-
+        return { ...result, ...newProduct }
     }
 
     static async update(id: number, request: Request) {
         const body = request.body as InsertProduct
-        const image = request.file!
+        const imageFile = request.files['image']?.[0] as Express.Multer.File
+        const zipFile = request.files["zip"]?.[0] as Express.Multer.File
 
-        const isImageExist = radash.isObject(image)
+        // validation image and zip
+        const isImage = radash.isObject(imageFile)
+        const isZip = radash.isObject(zipFile)
 
         // validation
         const productRequest = Validation.validate(ProductsValidation.ADD, body) as Product
+        const updateProductPayload = structuredClone(productRequest)
+
         // previous product
         const prevProduct = await ProductService.get(id)
 
-        let imageName: string, imagePath: string, imageBuffer: Buffer
-
         // if user upload new image
-        if (isImageExist) {
-            imageName = Date.now() + "-" + image.originalname
-            imageBuffer = image.buffer
-            imagePath = path.join(__dirname, "..", "..", "public", "static", imageName);
-            // add new image
+        if (isImage) {
+            const imageName = `${Date.now()}-${imageFile.originalname}`
+            const imageBuffer = imageFile.buffer
+            const imagePath = path.join(__dirname, "..", "..", "public", "static", imageName);
+            // insert new image
             await writeFile(imagePath, imageBuffer)
             // remove previous image
-            const prevImagePath = path.join(__dirname, "..", "..", "public", "static", prevProduct.image!)
+            const prevImagePath = path.join(__dirname, "..", "..", "public", "static", prevProduct.image)
             await unlink(prevImagePath)
+
+            updateProductPayload.image = imageName
         }
 
-        const updateProduct = {
-            ...productRequest,
-            ...(isImageExist ? { image: imageName } : { image: prevProduct.image }),
-            updatedAt: sql`NOW()`,
+        // if user upload new zip file
+        if (isZip) {
+            const zipName = `${Date.now()}-${zipFile.originalname}`
+            const zipBuffer = imageFile.buffer
+            const zipPath = path.join(__dirname, "..", "..", "_zip", zipName);
+            // insert new zip
+            await writeFile(zipPath, zipBuffer)
+            // generate new MD%
+            const zipMd5 = await generateMD5(zipPath)
+            // remove previous zip
+            const prevZipPath = path.join(__dirname, "..", "..", "_zip", prevProduct.zipPath)
+            await unlink(prevZipPath)
+
+            updateProductPayload.zipPath = zipName
+            updateProductPayload.zipMd5 = zipMd5
         }
+
+        updateProductPayload.updatedAt = sql`NOW()` as unknown as Date
+
 
         // if request body isOffer is true, force all data that have become isOffer=false
-        if (updateProduct.isOffer) {
+        if (updateProductPayload.isOffer) {
             await ProductService.forceIsOfferEqualIsFalse()
         }
 
         await db.update(productsTable)
-            .set(updateProduct)
+            .set(updateProductPayload)
             .where(eq(productsTable.id, id))
 
     }
@@ -148,8 +187,11 @@ export default class ProductService {
         // check are there product ?
         const product = await ProductService.get(id)
         // remove image from static dir
-        const imagePath = path.join(__dirname, "..", "..", "public", "static", product.image!)
+        const imagePath = path.join(__dirname, "..", "..", "public", "static", product.image)
+        const zipPath = path.join(__dirname, "..", "..", "_zip", product.zipPath)
+        
         await unlink(imagePath)
+        await unlink(zipPath)
 
         // if exist, remove it from db
         await db.delete(productsTable)
